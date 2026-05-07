a3d07a67fd6a900aa147ef332ef540403bae7d6b
robinrai1349 <robinrai1349@hotmail.com>
2026-05-07 09:05:00 +0100

    feat: stage migrants and exchange counts via alltoall

--- domain.cpp
+++ domain.cpp
@@ -88,3 +88,37 @@ void distribute_initial(int id, int p)
         }
     }
 }
+
+void migrate_particles(int id, int p)
+{
+    vector<vector<CParticle>> staging(p);
+
+    CParticle* current = CParticle::start;
+    while (current != nullptr)
+    {
+        CParticle* next_particle = current->next;
+        int dest = owner_of(current->x[0], p);
+
+        if (dest != id)
+        {
+            CParticle copy;
+            copy.v[0] = current->v[0]; copy.v[1] = current->v[1];
+            copy.x[0] = current->x[0]; copy.x[1] = current->x[1];
+            staging[dest].push_back(copy);
+
+            if (current->prev != nullptr) current->prev->next = current->next;
+            if (current->next != nullptr) current->next->prev = current->prev;
+            if (current == CParticle::start) CParticle::start = current->next;
+            delete current;
+        }
+
+        current = next_particle;
+    }
+
+    vector<int> send_counts(p, 0), recv_counts(p, 0);
+    for (int r = 0; r < p; r++)
+        send_counts[r] = static_cast<int>(staging[r].size());
+
+    MPI_Alltoall(send_counts.data(), 1, MPI_INT,
+                 recv_counts.data(), 1, MPI_INT, MPI_COMM_WORLD);
+}
